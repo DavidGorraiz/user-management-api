3f90b11ed63ce4f444405fd8718314e739f0a9c6
dmgorraiz <dmgorraiz@gmail.com>
2026-03-27 01:15:06 -0500

    feat: add User entity with JPA mapping

--- src/main/java/com/davidgorraiz/userapi/entity/User.java
+++ src/main/java/com/davidgorraiz/userapi/entity/User.java
@@ -1,4 +1,29 @@
 package com.davidgorraiz.userapi.entity;
-public class User {
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column(nullable = false, unique = true)
+    private String username;
+    @Column(nullable = false, unique = true)
+    private String email;
+    @Column(nullable = false)
+    private String password;
+    @Column(nullable = false)
+    private boolean enabled;
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt;
+    @Column(name = "last_login", nullable = false)
+    private LocalDateTime lastLogin;
 }
