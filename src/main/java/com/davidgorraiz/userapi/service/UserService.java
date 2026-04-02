4d8192c1f4100da751034dd676a7111e4013f114
dmgorraiz <dmgorraiz@gmail.com>
2026-04-01 22:01:47 -0500

    feat: Add a user service with the ability to view all users

--- src/main/java/com/davidgorraiz/userapi/service/UserService.java
+++ src/main/java/com/davidgorraiz/userapi/service/UserService.java
@@ -1,4 +1,21 @@
 package com.davidgorraiz.userapi.service;
 
+import com.davidgorraiz.userapi.dto.UserDTO;
+import com.davidgorraiz.userapi.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class UserService {
+
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public List<UserDTO> getAll() {
+        return this.userRepository.getAll();
+    }
 }
