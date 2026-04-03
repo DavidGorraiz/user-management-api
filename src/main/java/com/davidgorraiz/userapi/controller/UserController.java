9303bc19baf93e9700df3d7592fb1d485a3895fb
dmgorraiz <dmgorraiz@gmail.com>
2026-04-03 13:29:03 -0500

    feat: Add a controller that allows you to list users; in this case, its sole function is to list all users.

--- src/main/java/com/davidgorraiz/userapi/controller/UserController.java
+++ src/main/java/com/davidgorraiz/userapi/controller/UserController.java
@@ -1,4 +1,25 @@
 package com.davidgorraiz.userapi.controller;
 
+import com.davidgorraiz.userapi.dto.UserDTO;
+import com.davidgorraiz.userapi.service.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
 public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<UserDTO>> getAll(){
+        return ResponseEntity.ok(this.userService.getAll());
+    }
 }
