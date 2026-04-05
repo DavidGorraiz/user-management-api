fbb72c3144d84407bd444dc4b6b0a86c22a12141
dmgorraiz <dmgorraiz@gmail.com>
2026-04-05 18:28:16 -0500

    feat: add Role entity with JPA mapping

--- src/main/java/com/davidgorraiz/userapi/entity/Role.java
+++ src/main/java/com/davidgorraiz/userapi/entity/Role.java
@@ -1,4 +1,23 @@
 package com.davidgorraiz.userapi.entity;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "roles")
 public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    public Role(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Role() {
+    }
 }
