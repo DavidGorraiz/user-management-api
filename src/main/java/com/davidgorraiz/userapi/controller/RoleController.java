ec6e20193864e9abee1969d4124654dfb55a4fc8
dmgorraiz <dmgorraiz@gmail.com>
2026-04-07 12:05:01 -0500

    feat: Add a roles controller that allows you to view all roles

--- src/main/java/com/davidgorraiz/userapi/controller/RoleController.java
+++ src/main/java/com/davidgorraiz/userapi/controller/RoleController.java
@@ -1,4 +1,26 @@
 package com.davidgorraiz.userapi.controller;
 
+import com.davidgorraiz.userapi.dto.RoleDTO;
+import com.davidgorraiz.userapi.service.RoleService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/roles")
 public class RoleController {
+
+    private final RoleService roleService;
+
+    public RoleController(RoleService roleService) {
+        this.roleService = roleService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<RoleDTO>> getAll(){
+        return ResponseEntity.ok(this.roleService.getAll());
+    }
 }
