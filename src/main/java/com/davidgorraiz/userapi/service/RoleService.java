75b1982b92fa58093f1f48b1bbb5d30be056b55b
dmgorraiz <dmgorraiz@gmail.com>
2026-04-07 11:23:52 -0500

    feat: Add a role service that allows you to view all roles

--- src/main/java/com/davidgorraiz/userapi/service/RoleService.java
+++ src/main/java/com/davidgorraiz/userapi/service/RoleService.java
@@ -1,4 +1,21 @@
 package com.davidgorraiz.userapi.service;
 
+import com.davidgorraiz.userapi.dto.RoleDTO;
+import com.davidgorraiz.userapi.repository.RoleRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class RoleService {
+
+    private final RoleRepository roleRepository;
+
+    public RoleService(RoleRepository roleRepository) {
+        this.roleRepository = roleRepository;
+    }
+
+    public List<RoleDTO> getAll(){
+        return this.roleRepository.getAll();
+    }
 }
