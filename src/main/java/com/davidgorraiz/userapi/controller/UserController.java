24820255ea8a62fa0dd8f84c0063c04dc1a7c63c
dmgorraiz <dmgorraiz@gmail.com>
2026-05-05 14:06:28 -0500

    feat: Add a function to the user controller to create a new user

--- src/main/java/com/davidgorraiz/userapi/controller/UserController.java
+++ src/main/java/com/davidgorraiz/userapi/controller/UserController.java
@@ -2,11 +2,9 @@ package com.davidgorraiz.userapi.controller;
 
 import com.davidgorraiz.userapi.dto.UserDTO;
 import com.davidgorraiz.userapi.service.UserService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -27,5 +25,10 @@ public class UserController {
     @GetMapping("/{id}")
     public ResponseEntity<UserDTO> getById(@PathVariable long id) {
         return ResponseEntity.ok(this.userService.getById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<UserDTO> createUser(@RequestBody UserDTO userDTO){
+        return ResponseEntity.status(HttpStatus.CREATED).body(this.userService.createUser(userDTO));
     }
 }
