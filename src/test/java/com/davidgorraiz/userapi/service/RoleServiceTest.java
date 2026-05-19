797a19ac0832be777ddb675c9e20b0a4720d6cf7
dmgorraiz <dmgorraiz@gmail.com>
2026-05-19 16:43:22 -0500

    feat: Add a test to verify the method for searching by ID in the service layer for roles, and also to check whether it throws the custom exception if the ID is not found

--- src/test/java/com/davidgorraiz/userapi/service/RoleServiceTest.java
+++ src/test/java/com/davidgorraiz/userapi/service/RoleServiceTest.java
@@ -1,17 +1,22 @@
 package com.davidgorraiz.userapi.service;
 
 import com.davidgorraiz.userapi.dto.RoleDTO;
+import com.davidgorraiz.userapi.exceptions.RoleNotFoundException;
 import com.davidgorraiz.userapi.repository.RoleRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.verify;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.List;
+import java.util.Optional;
 
 @ExtendWith(MockitoExtension.class)
 public class RoleServiceTest {
@@ -22,14 +27,14 @@ public class RoleServiceTest {
     @InjectMocks
     private RoleService roleService;
 
+    // Arrange
+    List<RoleDTO> roles = List.of(
+            new RoleDTO(1L, "Admin"),
+            new RoleDTO(2L, "User")
+    );
+
     @Test
     void shouldReturnAllRoles(){
-        // Arrange
-        List<RoleDTO> roles = List.of(
-                new RoleDTO(1L, "Admin"),
-                new RoleDTO(2L, "User")
-        );
-
         when(roleRepository.getAll()).thenReturn(roles);
 
         // Act
@@ -41,5 +46,28 @@ public class RoleServiceTest {
         assertEquals("Admin", result.get(0).name());
 
         verify(roleRepository).getAll();
+    }
+    @Test
+    void shouldReturnRoleById(){
+        when(roleRepository.getById(roles.get(1).id())).thenReturn(Optional.ofNullable((roles.get(1))));
+
+        RoleDTO result = roleService.getById(roles.get(1).id());
+        System.out.println(result);
+
+        assertThat(result).isNotNull();
+        assertEquals(result.name(), roles.get(1).name());
+
+        verify(roleRepository).getById(roles.get(1).id());
+    }
+    @Test
+    void shouldReturnErrorNotFound(){
+        long id = 999L;
+        when(roleRepository.getById(id)).thenReturn(Optional.empty());
+
+        RoleNotFoundException ex = assertThrows(RoleNotFoundException.class,
+                () -> roleService.getById(id));
+        System.out.println(ex);
+
+        assertEquals("Role with id: 999 not found", ex.getMessage());
     }
 }
